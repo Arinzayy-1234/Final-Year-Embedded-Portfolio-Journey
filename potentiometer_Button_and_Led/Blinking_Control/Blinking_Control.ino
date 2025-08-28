f0bb52a922fdd760638cd4c0f3db7cb9da493eae
Arinzayyy - Kris <103210483+Arinzayy-1234@users.noreply.github.com>
2025-08-28 16:21:31 +0100

    Added the Blinking_Control file

---
+++ potentiometer_Button_and_Led/Blinking_Control/Blinking_Control.ino
@@ -0,0 +1,21 @@
+//put your setup code here, to run once:
+int ledPin = 9;
+int potPin = A0;
+void setup(){
+  pinMode(ledPin,OUTPUT);
+  pinMode(potPin,INPUT);
+}
+
+// put your main code here, to run repeatedly:
+void loop(){
+
+  int potValue = analogRead(potPin);
+  digitalWrite(ledPin,HIGH);
+  delay(potValue);
+
+  potValue = analogRead(potPin);
+  digitalWrite(ledPin, LOW);
+  delay(potValue);
+
+  
+}
