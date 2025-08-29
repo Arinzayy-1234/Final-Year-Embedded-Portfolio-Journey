05894347f17b7f223695743188e162d429c5e8e1
Arinzayyy - Kris <103210483+Arinzayy-1234@users.noreply.github.com>
2025-08-29 22:07:06 +0100

    ledFader Project -> For loops !!! -> added a project, where the led slowly fades from max to min brightness and vicerversa.

---
+++ ledFader/ledFader.ino
@@ -0,0 +1,30 @@
+
+int ledPin = 9;
+
+int rampTime = 2; //  controls the speed of the brightness change for the LED
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(ledPin,OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  //increase LED brightness to the max
+  for ( int i = 0 ; i <= 255 ; i ++)
+  {
+    analogWrite(ledPin, i);
+    delay(rampTime);
+    Serial.println(i);
+    delay(200);
+  }
+  //reduce LED brightness to the minimum
+  for (int i = 255; i >= 0; i--)
+  {
+    analogWrite(ledPin,i);
+    delay(rampTime);
+    Serial.println(i);
+    delay(200);
+  }
+}
