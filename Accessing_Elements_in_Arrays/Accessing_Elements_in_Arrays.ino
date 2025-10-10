e7b5b795fc906be19fc0306a9227ce9628d6c7e6
Arinzayyy - Kris <103210483+Arinzayy-1234@users.noreply.github.com>
2025-10-10 15:56:25 +0100

    Array Project: Array Handling -  Used for loops to access the elements of an array in 2 kind of ways 😌

---
+++ Accessing_Elements_in_Arrays/Accessing_Elements_in_Arrays.ino
@@ -0,0 +1,29 @@
+
+char arrayOne[6] = {"HELLO"};
+
+// All initialization and code that should run once must go here.
+void setup() {
+
+  Serial.begin(9600);
+  
+  for (char character : arrayOne)
+{
+  Serial.print("Char: ");
+  Serial.println(character);
+
+  Serial.print("ASCII:"); 
+  Serial.println((int)character); 
+}   
+
+
+{
+  Serial.print(arrayOne[i]);
+}
+
+}
+
+
+void loop() {
+
+
+}
---
+++ Accessing_Elements_in_Arrays/README.md
@@ -0,0 +1,1 @@
+Accessing_Elements_in_Arrays
